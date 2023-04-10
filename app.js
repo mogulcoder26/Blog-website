e141a575bf2363f28a8e68dec895055ce718062d
Soubhik <b422056@iiit-bh.ac.in>
2023-04-10 06:02:31 +0530

    added recursive functionality of adding blogs

--- app.js
+++ app.js
@@ -12,13 +12,32 @@ const homeText="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores
 const day1Text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores modi fuga rem, nesciunt molestias perspiciatis cupiditate hic. Facilis non pariatur perspiciatis saepe quos quo id cum repellendus qui amet, quasi, velit hic. Explicabo aspernatur, hic ipsa soluta ab neque assumenda provident, architecto inventore debitis consequatur molestias error, labore quidem tenetur unde. Deleniti delectus, culpa odio reprehenderit suscipit dolorem magnam? Deserunt iusto dolore facere molestias quod veniam, eaque quidem quia sapiente placeat minima voluptatem, fugiat unde non repudiandae corrupti!"
 const day2Text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores modi fuga rem, nesciunt molestias perspiciatis cupiditate hic. Facilis non pariatur perspiciatis saepe quos quo id cum repellendus qui amet, quasi, velit hic. Explicabo aspernatur, hic ipsa soluta ab neque assumenda provident, architecto inventore debitis consequatur molestias error, labore quidem tenetur unde. Deleniti delectus, culpa odio reprehenderit suscipit dolorem magnam? Deserunt iusto dolore facere molestias quod veniam, eaque quidem quia sapiente placeat minima voluptatem, fugiat unde non repudiandae corrupti!"
 
-
+let ALLPOSTS =[]
 app.get('/',(req,res)=>{
-    res.render('home.ejs',{htext:homeText,d1text:day1Text,d2text:day2Text})
+    console.log(ALLPOSTS)
+    res.render('home.ejs',{htext:homeText,d1text:day1Text,d2text:day2Text,blogsadd:ALLPOSTS})
 })
 
+app.get('/contact-me',(req,res)=>{
+    res.render('contact-me.ejs')
+})
 
+app.get('/about',(req,res)=>{
+    res.render('about.ejs')
+})
 
+app.get('/compose',(req,res)=>{
+    res.render('compose.ejs')
+})
+app.post('/',(req,res)=>{
+
+    let post={
+        title:req.body.titletxt,
+        blog:req.body.composetxt
+    }
+    ALLPOSTS.push(post);
+    res.redirect('/')
+})
 app.listen(process.env.PORT||port,()=>{
     console.log(`Server live at vercel | ${port}`)
 })
